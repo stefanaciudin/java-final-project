9e6d495430de7f72ff9ac13e58290a586d59a633
stefanaciudin <stefanaciudin@gmail.com>
2023-07-11 14:38:30 +0300

    modified readme file and added database creation script

---
+++ DatabaseImporter/database.sql
@@ -0,0 +1,124 @@
+create database spotifydb;
+create table playlist_names
+(
+    id   int auto_increment
+        primary key,
+    name varchar(50) not null
+);
+
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (1, 'Funny Playlist 1');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (2, 'Hilarious Hits');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (3, 'Laugh Out Loud');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (4, 'Comedy Central');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (5, 'Humor Me');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (6, 'Jokes and Jams');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (7, 'Giggle Groove');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (8, 'Chuckling Charts');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (9, 'Smile and Shuffle');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (10, 'Witty Beats');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (11, 'Cackle Collection');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (12, 'Humorous Harmonies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (13, 'Amusing Anthems');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (14, 'Lol-tunes');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (15, 'Whimsical Melodies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (16, 'Quirky Quavers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (17, 'Guffaw Grooves');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (18, 'Cheerful Chords');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (19, 'Chuckles and Chimes');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (20, 'Giggle Gurus');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (21, 'Funny Bone Bops');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (22, 'Jovial Jingles');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (23, 'Snicker Songs');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (24, 'Playful Playlists');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (25, 'Comical Compilation');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (26, 'Grin-tastic Tracks');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (27, 'Mirthful Mix');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (28, 'Happy Hilarity');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (29, 'Whistle-worthy Whimsies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (30, 'Chuckle Chart-toppers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (31, 'Quip-filled Queue');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (32, 'Giggle Galore');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (33, 'Merry Music');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (34, 'Laughing Lyrics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (35, 'Hysterical Harmony');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (36, 'Humorous Hits');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (37, 'Funny Favourites');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (38, 'Snicker Sounds');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (39, 'Witty Playlist');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (40, 'Giggle Gems');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (41, 'Comedy Classics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (42, 'Humor Harmonies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (43, 'Lighthearted Lyrics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (44, 'Quirk-filled Queue');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (45, 'Jester Jams');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (46, 'Witty Whistling');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (47, 'Giggle-rific Grooves');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (48, 'Chuckle Chords');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (49, 'Whimsical Whistle');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (50, 'Cheerful Comedy');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (51, 'Funny Feels');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (52, 'Smiling Soundtrack');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (53, 'Joke-filled Jams');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (54, 'Playful Picks');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (55, 'Amusing Artists');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (56, 'Lol-worthy Lyrics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (57, 'Guffaw Gems');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (58, 'Chuckle Collection');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (59, 'Comedy Cuts');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (60, 'Hilarious Harmonies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (61, 'Humorous Hymns');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (62, 'Witty Whistlers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (63, 'Laughing Melodies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (64, 'Whistle While You Listen');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (65, 'Giggle-filled Grooves');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (66, 'Funny Feats');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (67, 'Snicker Soundscape');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (68, 'Cheeky Compilation');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (69, 'Jovial Jams');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (70, 'Playful Performances');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (71, 'Amusing Anthems');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (72, 'Lighthearted Lyrics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (73, 'Quirky Quips');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (74, 'Comical Cuts');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (75, 'Humor Hits');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (76, 'Witty Whistleblowers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (77, 'Laughable Licks');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (78, 'Joke Jingles');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (79, 'Giggle Groovers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (80, 'Chuckling Chart-toppers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (81, 'Smile and Sing');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (82, 'Whimsical Whistling');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (83, 'Cheerful Compositions');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (84, 'Chucklesome Chords');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (85, 'Guffaw Gems');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (86, 'Playful Playlists');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (87, 'Comedic Compilation');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (88, 'Humor-filled Hits');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (89, 'Witty Whistlers');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (90, 'Laughing Lyrics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (91, 'Hysterical Harmony');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (92, 'Humorous Hits');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (93, 'Funny Favourites');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (94, 'Snicker Sounds');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (95, 'Witty Playlist');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (96, 'Giggle Gems');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (97, 'Comedy Classics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (98, 'Humor Harmonies');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (99, 'Lighthearted Lyrics');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (100, 'Quirk-filled Queue');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (101, 'Jester Jams');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (102, 'Witty Whistling');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (103, 'Giggle-rific Grooves');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (104, 'Chuckle Chords');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (105, 'Whimsical Whistle');
+INSERT INTO spotifydb.playlist_names (id, name) VALUES (106, 'Cheerful Comedy');
+
+
+create table top_songs
+(
+    id          int auto_increment
+        primary key,
+    artist_name varchar(150) null,
+    song_name   varchar(150) null
+);
+
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-hi
+# Java final project - using Spotify API
+
+Final project I made for my Advanced Programming class. The project uses a Java wrapper for Spotify's web API, available
+here:
+https://github.com/spotify-web-api-java/spotify-web-api-java
+
+## Backend
+
+The backend of this application is a Java Spring Boot application. Using the Spotify API, the application is capable to
+deliver different types of information about the current user:
+
+- Top artists (short term, medium term and long term) and the user's followed artists on Spotify
+- Top tracks (short term, medium term and long term)
+- Recently played tracks
+- Top genres (short term, medium term and long term)
+- The user's playlists
+
+The application can also create playlists, containing the user's top tracks for the specified period of time, or
+playlists containing artists that are related to the user's top artists - this way, the user can discover new music. The
+playlist names are selected randomly from the project's database.
+The user's top tracks are also compared to a list of 11400 most listened songs on Spotify (this tracks exist in the
+project database and are imported using the DatabaseImporter). This way, the user is given a percentage of how
+mainstream their music taste is.
+
+## Frontend
+
+The frontend of this application is a React application, that uses Tailwind CSS. It uses the backend to display the
+user's information and to create playlists.
+I used the Cattpuccin Mocha theme color palette, available here: https://github.com/catppuccin/catppuccin
+
+
+
